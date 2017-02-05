84c5dcf7618489701dbfc753950a2c01b0813a25
peizhouyu <peizhouyu@live.com>
2017-02-05 22:58:14 +0800

    实现查询功能

---
+++ app/src/main/java/com/free/freeweather/util/queryAPI.java
@@ -0,0 +1,11 @@
+package com.free.freeweather.util;
+
+/**
+ * Created by 裴周宇 on 2017/2/5.
+ */
+
+
+//天气查询API
+public class queryAPI {
+    public static final String query1 = "http://guolin.tech/api/china/";
+}
